bb40bd077bd96016a4bb8a5454318f433a465a7f
Sential <sentialx@gmail.com>
2019-06-16 19:18:53 +0200

    feat: add WindowsButtons and WindowsButton components

---
+++ src/components/WindowsControl/index.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+
+import closeIcon from "../../icons/close.svg";
+import maximizeIcon from "../../icons/maximize.svg";
+import minimizeIcon from "../../icons/minimize.svg";
+
+interface Props {
+  maximize?: boolean;
+  close?: boolean;
+  minimize?: boolean;
+  whiteIcon?: boolean;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+interface State {
+  hover?: boolean;
+}
+
+export class WindowsControl extends React.PureComponent<Props, State> {
+  onMouseEnter = () => {
+    this.setState({ hover: true });
+  };
+
+  onMouseLeave = () => {
+    this.setState({ hover: false });
+  };
+
+  public render() {
+    const { hover } = this.state;
+    const { close, maximize, minimize, whiteIcon, onClick } = this.props;
+
+    let icon: string;
+
+    if (close) icon = closeIcon;
+    else if (maximize) icon = maximizeIcon;
+    else if (minimize) icon = minimizeIcon;
+
+    return (
+      <div
+        onClick={onClick}
+        style={{
+          height: "100%",
+          width: 45,
+          minWidth: 45,
+          position: "relative",
+          transition: "0.2s background-color",
+          "-webkit-app-region": "no-drag",
+          backgroundColor: hover
+            ? !close
+              ? "rgba(196, 196, 196, 0.4)"
+              : "#e81123"
+            : "transparent"
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            transition: "0.2s filter",
+            filter: whiteIcon || (close && hover) ? "invert(100%)" : "none",
+            backgroundPosition: "center",
+            backgroundSize: "11px",
+            backgroundRepeat: "no-repeat",
+            backgroundImage: `url(${icon})`
+          }}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/WindowsControls/index.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { WindowsControl } from "../WindowsControl";
+
+interface Props {
+  dark?: boolean;
+  onClose?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  onMaximize?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  onMinimize?: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+export const WindowsButtons = ({
+  dark,
+  onClose,
+  onMaximize,
+  onMinimize
+}: Props) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <WindowsControl minimize whiteIcon={dark} onClick={onMinimize} />
+      <WindowsControl maximize whiteIcon={dark} onClick={onMaximize} />
+      <WindowsControl close whiteIcon={dark} onClick={onClose} />
+    </div>
+  );
+};
